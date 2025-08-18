9949e72ba4477625a8ac75459b1a6921c7250a4b
invi1998 <2267661826@qq.com>
2025-08-18 23:52:35 +0800

    设置基类

---
+++ Source/Arenas/Private/player/PlayerArenasCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerArenasCharacter.h"
+
+
+// Sets default values
+APlayerArenasCharacter::APlayerArenasCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerArenasCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerArenasCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlayerArenasCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
