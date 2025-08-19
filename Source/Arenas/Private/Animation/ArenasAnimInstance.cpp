d22277bff6bc3d09fbc9f8da5afd037064674c1d
invi1998 <2267661826@qq.com>
2025-08-19 19:14:03 +0800

    创建动画实例

---
+++ Source/Arenas/Private/Animation/ArenasAnimInstance.cpp
@@ -0,0 +1,30 @@
+﻿// Ace of Arenas. (invi1998 All Rights Reserved)
+
+
+#include "ArenasAnimInstance.h"
+
+#include "GameFramework/Character.h"
+
+void UArenasAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerPlayerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerPlayerCharacter)
+	{
+		OwnerMovementComponent = OwnerPlayerCharacter->GetCharacterMovement();
+	}
+	else
+	{
+		OwnerMovementComponent = nullptr;
+	}
+	
+}
+
+void UArenasAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+}
+
+void UArenasAnimInstance::NativeUninitializeAnimation()
+{
+}
---
+++ Source/Arenas/Private/Animation/ArenasAnimInstance.h
@@ -0,0 +1,32 @@
+﻿// Ace of Arenas. (invi1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ArenasAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UArenasAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeUninitializeAnimation() override;
+	
+protected:
+
+private:
+	UPROPERTY()
+	ACharacter* OwnerPlayerCharacter;
+
+	UPROPERTY()
+	UCharacterMovementComponent* OwnerMovementComponent;
+	
+};
