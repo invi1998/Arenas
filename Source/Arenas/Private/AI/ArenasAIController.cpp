15472cca23be9d759cffa3c042391c91a1a3291e
invi <2267661826@qq.com>
2025-08-30 22:35:43 +0800

    添加 AI 并配置 AI 的 TeamID 与视野感知

---
+++ Source/Arenas/Private/AI/ArenasAIController.cpp
@@ -0,0 +1,46 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "ArenasAIController.h"
+
+#include "Character/ArenasCharacter.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+// Sets default values
+AArenasAIController::AArenasAIController()
+{
+	PerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComp"));
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;		// 只检测敌人
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;	// 不检测中立
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;	// 不检测友方
+
+	SightConfig->SightRadius = 1000.f;			// 视野范围
+	SightConfig->LoseSightRadius = 1200.f;		// 失去视野范围
+	SightConfig->PeripheralVisionAngleDegrees = 180.f; // 视野角度
+
+	SightConfig->SetMaxAge(5.f);				// 记忆时间
+
+	PerceptionComp->ConfigureSense(*SightConfig);
+	PerceptionComp->SetDominantSense(SightConfig->GetSenseImplementation());	// 设置主要感官为视觉
+	
+}
+
+void AArenasAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	SetGenericTeamId(FGenericTeamId(0)); // 设置为队伍0
+
+	if (IGenericTeamAgentInterface* PawnAsTeamAgent = Cast<IGenericTeamAgentInterface>(InPawn))
+	{
+		PawnAsTeamAgent->SetGenericTeamId(GetGenericTeamId()); // 让被控制的Pawn也属于同一队伍
+	}
+	
+}
+
+
+
---
+++ Source/Arenas/Private/AI/ArenasAIController.h
@@ -0,0 +1,33 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ArenasAIController.generated.h"
+
+class UAISenseConfig_Sight;
+class UAIPerceptionComponent;
+
+UCLASS()
+class ARENAS_API AArenasAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AArenasAIController();
+	virtual void OnPossess(APawn* InPawn);
+
+protected:
+
+private:
+	// 感知组件
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	UAIPerceptionComponent* PerceptionComp;
+
+	// 视觉感知配置
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	UAISenseConfig_Sight* SightConfig;
+
+};
