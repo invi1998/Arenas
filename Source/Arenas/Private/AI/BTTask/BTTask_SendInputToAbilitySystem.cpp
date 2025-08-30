e015f31dd55e14d4f0b92783aa11b4c5aea2e961
invi <2267661826@qq.com>
2025-08-31 02:49:57 +0800

    使 AI 进行攻击

---
+++ Source/Arenas/Private/AI/BTTask/BTTask_SendInputToAbilitySystem.cpp
@@ -0,0 +1,24 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "BTTask_SendInputToAbilitySystem.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AIController.h"
+
+EBTNodeResult::Type UBTTask_SendInputToAbilitySystem::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (AAIController* AIController = OwnerComp.GetAIOwner())
+	{
+		if (UAbilitySystemComponent* OwnerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(AIController->GetPawn()))
+		{
+			OwnerASC->PressInputID(static_cast<int32>(AbilityInputID));
+			return EBTNodeResult::Succeeded;
+		}
+	}
+
+	return EBTNodeResult::Failed;
+
+	
+}
---
+++ Source/Arenas/Private/AI/BTTask/BTTask_SendInputToAbilitySystem.h
@@ -0,0 +1,25 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Types/ArenaStructTypes.h"
+#include "BTTask_SendInputToAbilitySystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UBTTask_SendInputToAbilitySystem : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Ability")
+	EArenasAbilityInputID AbilityInputID = EArenasAbilityInputID::None;
+	
+};
