406b843b1cdcde2da7c7ee05d55c68dcebd882e5
invi <2267661826@qq.com>
2025-09-01 01:57:12 +0800

    创建小兵兵营类并实现基础小兵生成

---
+++ Source/Arenas/Private/AI/MinionBarrack.cpp
@@ -0,0 +1,75 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "MinionBarrack.h"
+
+#include "Character/Minion/MinionCharacter.h"
+#include "GameFramework/PlayerStart.h"
+
+
+// Sets default values
+AMinionBarrack::AMinionBarrack()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMinionBarrack::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnMinionsToPool(InitialMinionPoolSize);
+	
+}
+
+// Called every frame
+void AMinionBarrack::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+const APlayerStart* AMinionBarrack::GetNextSpawnPoint()
+{
+	if (MinionSpawnPoints.Num() > 0)
+	{
+		++NextSpawnSpotIndex;
+
+		if (NextSpawnSpotIndex >= MinionSpawnPoints.Num())
+		{
+			NextSpawnSpotIndex = 0;
+		}
+
+		return MinionSpawnPoints[NextSpawnSpotIndex];
+	}
+
+	return nullptr;
+}
+
+void AMinionBarrack::SpawnMinionsToPool(int32 NumMinions)
+{
+	for (int32 i = 0; i < NumMinions; ++i)
+	{
+		FTransform SpawnPointTransform = GetActorTransform();
+		if (const APlayerStart* SpawnPoint = GetNextSpawnPoint())
+		{
+			SpawnPointTransform = SpawnPoint->GetActorTransform();
+		}
+
+		AMinionCharacter* NewMinion = GetWorld()->SpawnActorDeferred<AMinionCharacter>(
+			MinionClass,
+			SpawnPointTransform,
+			this,
+			nullptr,
+			ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+
+		if (NewMinion)
+		{
+			NewMinion->SetGenericTeamId(BarrackTeamID);
+			NewMinion->FinishSpawning(SpawnPointTransform);
+			SpawnedMinionPool.Add(NewMinion);
+		}
+		
+	}
+}
+
---
+++ Source/Arenas/Private/AI/MinionBarrack.h
@@ -0,0 +1,55 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
+#include "GameFramework/Actor.h"
+#include "MinionBarrack.generated.h"
+
+// 兵营
+
+class AMinionCharacter;
+
+UCLASS()
+class ARENAS_API AMinionBarrack : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMinionBarrack();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 兵营所属队伍ID
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	FGenericTeamId BarrackTeamID;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	int32 InitialMinionPoolSize = 5; // 初始预生成的小兵数量
+
+	// 预生成的兵营小兵池
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TArray<AMinionCharacter*> SpawnedMinionPool;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TSubclassOf<AMinionCharacter> MinionClass;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TArray<APlayerStart *> MinionSpawnPoints;
+
+	int32 NextSpawnSpotIndex = -1;
+
+	const APlayerStart* GetNextSpawnPoint();
+
+	void SpawnMinionsToPool(int32 NumMinions);
+	
+};
