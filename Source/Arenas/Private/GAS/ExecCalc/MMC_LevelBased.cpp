254b39f895210294a7f5827e1efd29620364ca1b
invi <2267661826@qq.com>
2025-09-26 17:33:05 +0800

    使用等级驱动其他属性

---
+++ Source/Arenas/Private/GAS/ExecCalc/MMC_LevelBased.cpp
@@ -0,0 +1,36 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "MMC_LevelBased.h"
+
+UMMC_LevelBased::UMMC_LevelBased()
+{
+	RelevantAttributesToCapture.Add(GetMMCLevelBasedCaptureStatic().LevelDef);
+}
+
+float UMMC_LevelBased::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	UAbilitySystemComponent* SourceASC = Spec.GetContext().GetInstigatorAbilitySystemComponent();
+	if (!SourceASC)
+	{
+		return 0.f;
+	}
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	EvaluationParameters.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	// 获取等级属性值
+	float Level = 0.f;
+	GetCapturedAttributeMagnitude(GetMMCLevelBasedCaptureStatic().LevelDef, Spec, EvaluationParameters, Level);
+
+	bool bFound = false;
+	float RateAttributeValue = SourceASC->GetGameplayAttributeValue(RateAttribute, bFound);
+	if (!bFound)
+	{
+		return 0.f;
+	}
+
+	return (Level - 1) * RateAttributeValue;
+	
+}
---
+++ Source/Arenas/Private/GAS/ExecCalc/MMC_LevelBased.h
@@ -0,0 +1,46 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "GAS/ArenasAttributeSet.h"
+#include "GAS/ArenasHeroAttributeSet.h"
+#include "MMC_LevelBased.generated.h"
+
+struct FArenasLevelBasedCaptureStatics_MMC
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Level)
+
+	FArenasLevelBasedCaptureStatics_MMC()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UArenasHeroAttributeSet, Level, Source, true)
+	}
+	
+};
+
+static const FArenasLevelBasedCaptureStatics_MMC& GetMMCLevelBasedCaptureStatic()
+{
+	static FArenasLevelBasedCaptureStatics_MMC LevelStatics;
+	return LevelStatics;
+}
+
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UMMC_LevelBased : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UMMC_LevelBased();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayAttribute RateAttribute;
+	
+};
