9dd2426a20e9885ca29754f1a025d564d368252b
invi <2267661826@qq.com>
2025-09-29 21:25:10 +0800

    将 AN_SendTargetGroup 重构为在本地发送伤害 GameplayCue

--- Source/Arenas/Private/GAS/AnimNotify/AN_SendTargetGroup.cpp
+++ Source/Arenas/Private/GAS/AnimNotify/AN_SendTargetGroup.cpp
@@ -4,6 +4,10 @@
 #include "AN_SendTargetGroup.h"
 
 #include "AbilitySystemBlueprintLibrary.h"
+#include "GAS/ArenasAbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GameplayCueManager.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAN_SendTargetGroup::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                  const FAnimNotifyEventReference& EventReference)
@@ -17,21 +21,78 @@ void UAN_SendTargetGroup::Notify(USkeletalMeshComponent* MeshComp, UAnimSequence
 	if (!MeshComp->GetOwner() || !UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(MeshComp->GetOwner())) return;
 
 	FGameplayEventData PayloadData;
+	TSet<AActor*> HitActors;
+	AActor* OwnerActor = MeshComp->GetOwner();
+	const IGenericTeamAgentInterface* OwnerTeamAgent = Cast<IGenericTeamAgentInterface>(OwnerActor);
+
+	TArray<AActor*> IgnoredActors;
+	if (bIgnoreSelf)
+	{
+		IgnoredActors.Add(OwnerActor);
+	}
 	for (int32 i = 1; i < TargetSocketNames.Num(); ++i)
 	{
-		// 堆分配内存
-		FGameplayAbilityTargetData_LocationInfo* LocationInfo = new FGameplayAbilityTargetData_LocationInfo();
-
 		FVector StartLocation = MeshComp->GetSocketLocation(TargetSocketNames[i-1]);
 		FVector EndLocation = MeshComp->GetSocketLocation(TargetSocketNames[i]);
+		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+		ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
 
-		LocationInfo->SourceLocation.LiteralTransform.SetLocation(StartLocation);
-		LocationInfo->TargetLocation.LiteralTransform.SetLocation(EndLocation);
+		TArray<FHitResult> Hits;
 
-		// 添加到目标数据数组中，Add内部会使用智能指针进行管理，所以这里不需要担心内存泄漏
-		// 同时也需要注意，因为Add内部会使用智能指针进行管理，所以上面我们创建的LocationInfo必须是原始指针
-		PayloadData.TargetData.Add(LocationInfo);
+		UKismetSystemLibrary::SphereTraceMultiForObjects(
+			MeshComp,
+			StartLocation,
+			EndLocation,
+			SphereSweepRadius,
+			ObjectTypes,
+			false,
+			IgnoredActors,
+			bDrawDebug ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None,
+			Hits,
+			false
+		);
+
+		for (const FHitResult& Hit : Hits)
+		{
+			if (HitActors.Contains(Hit.GetActor()))
+			{
+				continue;
+			}
+			if (OwnerTeamAgent)
+			{
+				ETeamAttitude::Type OtherActorTeamAttitude = OwnerTeamAgent->GetTeamAttitudeTowards(*Hit.GetActor());
+				if (OtherActorTeamAttitude != TargetTeamType)
+				{
+					// 如果目标不符合指定的队伍关系，则跳过
+					continue;
+				}
+			}
+
+			HitActors.Add(Hit.GetActor());
+			PayloadData.TargetData.Add(new FGameplayAbilityTargetData_SingleTargetHit(Hit));
+
+			SendLocalGameplayCue(Hit);
+		}
 	}
 
 	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), EventTag, PayloadData);
+}
+
+void UAN_SendTargetGroup::SendLocalGameplayCue(const FHitResult& HitResult) const
+{
+	// 只在本地客户端发送GameplayCue，不需要服务器和其他客户端接收
+	FGameplayCueParameters Parameters;
+	Parameters.Location = HitResult.ImpactPoint;
+	Parameters.Normal = HitResult.ImpactNormal;
+
+	for (const FGameplayTag& CueTag : TriggerGameplayCueTags)
+	{
+		// 通过GameplayCueManager来处理GameplayCue
+		UAbilitySystemGlobals::Get().GetGameplayCueManager()->HandleGameplayCue(
+			HitResult.GetActor(),
+			CueTag,
+			EGameplayCueEvent::Executed,
+			Parameters
+		);
+	}
 }
--- Source/Arenas/Private/GAS/AnimNotify/AN_SendTargetGroup.h
+++ Source/Arenas/Private/GAS/AnimNotify/AN_SendTargetGroup.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
 #include "Animation/AnimNotifies/AnimNotify.h"
+#include "GenericTeamAgentInterface.h"
 #include "AN_SendTargetGroup.generated.h"
 
 /**
@@ -24,5 +25,22 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = "Gameplay Ability", meta=(Categories="Event", GameplayTagFilter = "Event"))
 	FGameplayTag EventTag;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	FGameplayTagContainer TriggerGameplayCueTags;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	TEnumAsByte<ETeamAttitude::Type> TargetTeamType = ETeamAttitude::Hostile;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bDrawDebug = false;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	float SphereSweepRadius = 60.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bIgnoreSelf = true;
+
+	void SendLocalGameplayCue(const FHitResult& HitResult) const;
 	
 };
