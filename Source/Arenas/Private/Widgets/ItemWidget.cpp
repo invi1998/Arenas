59954f2f5a054f9ddbf9b8c7d831abaa042518d4
invi <2267661826@qq.com>
2025-09-30 00:32:28 +0800

    Create and Test the Item Widget Class

---
+++ Source/Arenas/Private/Widgets/ItemWidget.cpp
@@ -0,0 +1,74 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "ItemWidget.h"
+
+#include "Components/Image.h"
+
+void UItemWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SetIsFocusable(true);
+}
+
+FReply UItemWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	FReply SuperReply = Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+
+	if (InMouseEvent.IsMouseButtonDown(EKeys::RightMouseButton))
+	{
+		// 如果是右键按下，此时要确保我们获取焦点，传入TakeWidget()，表示将焦点设置到当前Widget上
+		// 这样做的目的是为了让右键点击时，当前Widget能够接收键盘输入事件，同时这样做也能让其他被聚焦的Widget失去焦点
+		// 同时，还需要注意：这样做之后，会触发其他被聚焦的Widget的 ButtonUp 事件
+		return FReply::Handled().SetUserFocus(TakeWidget());
+	}
+
+	if (InMouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton))
+	{
+		// 如果是左键按下，此处我们希望控件能支持拖拽功能，所以在设置控件聚焦后，调用 DetectDrag 方法
+		// 该方法会检测用户是否在按下鼠标左键后移动了鼠标，如果移动了鼠标，则会触发拖拽操作
+		// 这里同样传入 TakeWidget()，表示将焦点设置到当前Widget上
+		return FReply::Handled().SetUserFocus(TakeWidget()).DetectDrag(TakeWidget(), EKeys::LeftMouseButton);
+	}
+
+	return SuperReply;
+	
+}
+
+FReply UItemWidget::NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	FReply SuperReply = Super::NativeOnMouseButtonUp(InGeometry, InMouseEvent);
+	if (HasAnyUserFocus())
+	{
+		// 如果当前Widget拥有焦点，且鼠标左键抬起，则表示用户完成了点击操作
+		if (InMouseEvent.GetEffectingButton() == EKeys::RightMouseButton)
+		{
+			RightButtonClicked();
+			return FReply::Handled();
+		}
+		if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+		{
+			LeftButtonClicked();
+			return FReply::Handled();
+		}
+	}
+
+	return SuperReply;
+}
+
+void UItemWidget::SetIcon(UTexture2D* IconTexture)
+{
+	ImageIcon->SetBrushFromTexture(IconTexture);
+}
+
+void UItemWidget::RightButtonClicked()
+{
+	// 在屏幕上打印日志
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Right Button Clicked"));
+}
+
+void UItemWidget::LeftButtonClicked()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Left Button Clicked"));
+}
---
+++ Source/Arenas/Private/Widgets/ItemWidget.h
@@ -0,0 +1,34 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemWidget.generated.h"
+
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	
+	virtual void SetIcon(UTexture2D* IconTexture);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UImage* ImageIcon;
+
+	virtual void RightButtonClicked();
+	virtual void LeftButtonClicked();
+
+	
+	
+};
