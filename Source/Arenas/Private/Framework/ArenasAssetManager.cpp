33c5fd95e9cbe5ba37ecfb49d1a8c6e6f876e5af
invi <2267661826@qq.com>
2025-09-29 23:53:37 +0800

    创建资源管理器和ShopItem加载函数

---
+++ Source/Arenas/Private/Framework/ArenasAssetManager.cpp
@@ -0,0 +1,86 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "ArenasAssetManager.h"
+
+#include "Inventory/PA_ShopItem.h"
+
+UArenasAssetManager& UArenasAssetManager::Get()
+{
+	if (UArenasAssetManager* Singleton = Cast<UArenasAssetManager>(GEngine->AssetManager.Get()))
+	{
+		return *Singleton;
+	}
+
+	UE_LOG(LogTemp, Fatal, TEXT("UArenasAssetManager::Get(): Singleton is null! Please make sure to set the AssetManagerClassName in DefaultEngine.ini to point to your custom AssetManager class."));
+	return *NewObject<UArenasAssetManager>();
+}
+
+void UArenasAssetManager::LoadShopItems(const FStreamableDelegate& LoadFinishedCallback)
+{
+	// 这里传入一个空的TArray<FName>，表示加载该类型的所有资源 （如果想支持一些高级功能，比如只加载某些特定的资源（如只加载图标等），可以传入一个包含这些资源名称的数组）
+	LoadPrimaryAssetsWithType(UPA_ShopItem::GetShopItemAssetType(), TArray<FName>(), FStreamableDelegate::CreateUObject(this, &UArenasAssetManager::ShopItemLoadCompleted, LoadFinishedCallback));
+}
+
+bool UArenasAssetManager::GetLoadedShopItems(TArray<const UPA_ShopItem*>& OutShopItems) const
+{
+	TArray<UObject*> LoadedObjects;
+	bool Loaded = GetPrimaryAssetObjectList(UPA_ShopItem::GetShopItemAssetType(), LoadedObjects);
+
+	if (Loaded)
+	{
+		for (UObject* Item : LoadedObjects)
+		{
+			OutShopItems.Add(Cast<UPA_ShopItem>(Item));
+		}
+	}
+
+	return Loaded;
+}
+
+void UArenasAssetManager::ShopItemLoadCompleted(FStreamableDelegate CallBack)
+{
+	CallBack.ExecuteIfBound();
+	BuildItemMaps();
+}
+
+void UArenasAssetManager::AddToCombinationMap(const UPA_ShopItem* Ingredient, const UPA_ShopItem* CombinationItem)
+{
+	FItemCollectionEntry* Combinations = CombinationMap.Find(Ingredient);
+	if (Combinations)
+	{
+		if (!Combinations->Contains(CombinationItem))
+		{
+			CombinationMap.Add(CombinationItem);
+		}
+	}
+	else
+	{
+		CombinationMap.Add(Ingredient, FItemCollectionEntry{TArray<const UPA_ShopItem*>{CombinationItem}});
+	}
+}
+
+void UArenasAssetManager::BuildItemMaps()
+{
+	// 遍历所有加载的UPA_ShopItem资源，并将它们填充在 CombinationMap （组合映射） 和 IngredientMap （成分映射） 中
+	TArray<const UPA_ShopItem*> LoadedShopItems;
+	if (GetLoadedShopItems(LoadedShopItems))
+	{
+		for (const UPA_ShopItem* ShopItem : LoadedShopItems)
+		{
+			if (ShopItem->GetIngredientItems().Num() == 0) continue;
+
+			TArray<const UPA_ShopItem*> ShopItems;
+			for (const TSoftObjectPtr<UPA_ShopItem>& IngredientItem : ShopItem->GetIngredientItems())
+			{
+				UPA_ShopItem* IngredientShopItem = IngredientItem.LoadSynchronous();
+				ShopItems.Add(IngredientShopItem);
+				AddToCombinationMap(IngredientShopItem, ShopItem);	// 将该物品添加到组合映射中
+			}
+
+			IngredientMap.Add(ShopItem, FItemCollectionEntry{ShopItems});
+			
+		}
+	}
+}
+
---
+++ Source/Arenas/Private/Framework/ArenasAssetManager.h
@@ -0,0 +1,39 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "Inventory/PA_ShopItem.h"
+#include "ArenasAssetManager.generated.h"
+
+class UPA_ShopItem;
+/**
+ * 基类是一个单例类，继承自UAssetManager，用于管理游戏中的各种资源。
+ * 通过这个类，可以方便地加载、卸载和管理游戏中的各种资源，这就是我们的物品栏，商店等类型的资源管理。
+ * 该类通常在游戏启动时初始化，并在整个游戏生命周期中保持存在。
+ */
+UCLASS()
+class ARENAS_API UArenasAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	static UArenasAssetManager& Get();
+	void LoadShopItems(const FStreamableDelegate& LoadFinishedCallback);
+	bool GetLoadedShopItems(TArray<const UPA_ShopItem*>& OutShopItems) const;
+
+private:
+	void ShopItemLoadCompleted(FStreamableDelegate CallBack);
+	void BuildItemMaps();
+	void AddToCombinationMap(const UPA_ShopItem* Ingredient, const UPA_ShopItem* CombinationItem);
+
+	// 物品合成映射表，Key为合成后的物品，Value为合成该物品所需的材料和数量
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollectionEntry> CombinationMap;
+
+	// 物品合成映射表，Key为材料物品，Value为使用该材料可以合成的物品和数量
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollectionEntry> IngredientMap;
+	
+};
