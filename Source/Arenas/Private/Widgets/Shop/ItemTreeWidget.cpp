13bb17db40de37818ae03123887399ce9053e9fa
invi <2267661826@qq.com>
2025-10-04 20:08:00 +0800

    创建物品树小部件的参数和实用函数

--- Source/Arenas/Private/Widgets/Shop/ItemTreeWidget.cpp
+++ Source/Arenas/Private/Widgets/Shop/ItemTreeWidget.cpp
@@ -2,3 +2,48 @@
 
 
 #include "ItemTreeWidget.h"
+
+#include "SplineWidget.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+
+void UItemTreeWidget::ClearTree()
+{
+	RootPanel->ClearChildren();
+}
+
+UUserWidget* UItemTreeWidget::CreateWidgetForNode(const ITreeNodeInterface* Node, UCanvasPanelSlot*& OutCanvasPanelSlot)
+{
+	if (!Node) return nullptr;
+
+	UUserWidget* NodeWidget = Node->GetWidget();
+	OutCanvasPanelSlot = RootPanel->AddChildToCanvas(NodeWidget);
+	if (OutCanvasPanelSlot)
+	{
+		OutCanvasPanelSlot->SetSize(NodeSize);
+		OutCanvasPanelSlot->SetAnchors(FAnchors(0.5f, 0.5f));		// 设置锚点为中心
+		OutCanvasPanelSlot->SetAlignment(FVector2D(0.5f, 0.5f));	// 设置对齐方式为中心
+		OutCanvasPanelSlot->SetZOrder(1);
+	}
+
+	return NodeWidget;
+	
+}
+
+void UItemTreeWidget::CreateConnection(const UUserWidget* From, const UUserWidget* To)
+{
+	if (!From || !To) return;
+
+	USplineWidget* ConnectionSpline = CreateWidget<USplineWidget>(GetOwningPlayer());
+	if (UCanvasPanelSlot* ConnectionPanelSlot = RootPanel->AddChildToCanvas(ConnectionSpline))
+	{
+		ConnectionPanelSlot->SetAnchors(FAnchors(0.f));		// 设置锚点为左上角
+		ConnectionPanelSlot->SetAlignment(FVector2D(0.f, 0.f));
+		ConnectionPanelSlot->SetPosition(FVector2D::ZeroVector);
+		ConnectionPanelSlot->SetZOrder(0);
+	}
+
+	ConnectionSpline->SetupSpline(From, To, SourcePortLocationPos, DestinationPortDirection, SourcePortDirection, DestinationPortDirection);
+	ConnectionSpline->SetSplineStyle(ConnectionColor, ConnectionThickness);
+	
+}
--- Source/Arenas/Private/Widgets/Shop/ItemTreeWidget.h
+++ Source/Arenas/Private/Widgets/Shop/ItemTreeWidget.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Interface/TreeNodeInterface.h"
 #include "ItemTreeWidget.generated.h"
 
+class UCanvasPanelSlot;
+class UCanvasPanel;
 /**
  * 
  */
@@ -13,4 +16,38 @@ UCLASS()
 class ARENAS_API UItemTreeWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+
+private:
+	void ClearTree();
+	UUserWidget* CreateWidgetForNode(const ITreeNodeInterface* Node, UCanvasPanelSlot*& OutCanvasPanelSlot);
+	void CreateConnection(const UUserWidget* From, const UUserWidget* To);
+	
+	UPROPERTY(meta=(BindWidget))
+	UCanvasPanel* RootPanel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FVector2D NodeSize = FVector2D(50.f, 50.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FVector2D NodeGap = FVector2D(16.f, 30.f);
+
+	// 连接线颜色 (默认橘黄色）
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FLinearColor ConnectionColor = FLinearColor{1.f, 0.647f, 0.f, 1.f};
+
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	float ConnectionThickness = 4.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FVector2D SourcePortLocationPos = FVector2D(0.5f, 0.1f);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FVector2D SourcePortDirection = FVector2D(0.f, 90.f);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "ItemTree")
+	FVector2D DestinationPortDirection = FVector2D(0.f, 90.f);
+	
 };
