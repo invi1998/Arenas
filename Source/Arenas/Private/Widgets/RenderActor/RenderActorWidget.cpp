08c6b58541b0b45120547e0ae334d2ae8bc44e6c
invi <2267661826@qq.com>
2025-10-06 13:49:13 +0800

    创建渲染器角色小部件类

---
+++ Source/Arenas/Private/Widgets/RenderActor/RenderActorWidget.cpp
@@ -0,0 +1,80 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "RenderActorWidget.h"
+
+#include "RenderActor.h"
+#include "Components/Image.h"
+#include "Components/SizeBox.h"
+#include "Engine/TextureRenderTarget2D.h"
+
+void URenderActorWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	RenderSizeBox->SetWidthOverride(RenderSize.X);
+	RenderSizeBox->SetHeightOverride(RenderSize.Y);
+	
+}
+
+void URenderActorWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SpawnRenderActor();
+	ConfigureRenderActor();
+	BeginRenderCapture();
+	
+}
+
+void URenderActorWidget::BeginDestroy()
+{
+	StopRenderCapture();
+	
+	Super::BeginDestroy();
+}
+
+void URenderActorWidget::ConfigureRenderActor()
+{
+	if (!GetRenderActor())
+	{
+		UE_LOG(LogTemp, Error, TEXT("RenderActorWidget is NULL"));
+		return;
+	}
+
+	RenderTarget = NewObject<UTextureRenderTarget2D>(this);
+	RenderTarget->InitAutoFormat(static_cast<uint32>(RenderSize.X), static_cast<uint32>(RenderSize.Y));
+	RenderTarget->RenderTargetFormat = ETextureRenderTargetFormat::RTF_RGBA8_SRGB;	// 使用sRGB格式以确保颜色正确显示
+	GetRenderActor()->SetRenderTarget(RenderTarget);
+
+	if (UMaterialInstanceDynamic* DisplayDynamicImageMID = DisplayImage->GetDynamicMaterial())
+	{
+		DisplayDynamicImageMID->SetTextureParameterValue(DisplayImageRenderTargetParamName, RenderTarget);
+	}
+	
+}
+
+void URenderActorWidget::BeginRenderCapture()
+{
+	RenderTickInterval = 1.f / static_cast<float>(FrameRate);
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().SetTimer(RenderTickTimerHandle, this, &URenderActorWidget::UpdateRender, RenderTickInterval, true);
+	}
+}
+
+void URenderActorWidget::UpdateRender()
+{
+	if (GetRenderActor())
+	{
+		GetRenderActor()->UpdateRender();
+	}
+}
+
+void URenderActorWidget::StopRenderCapture()
+{
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(RenderTickTimerHandle);
+	}
+}
---
+++ Source/Arenas/Private/Widgets/RenderActor/RenderActorWidget.h
@@ -0,0 +1,58 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RenderActorWidget.generated.h"
+
+class ARenderActor;
+class USizeBox;
+class UImage;
+class UTextureRenderTarget2D;
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class ARENAS_API URenderActorWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	virtual void BeginDestroy() override;
+	
+private:
+	// 如果想在UUserWidget中定义一个纯虚函数，该类必须是一个Abstract（抽象类）
+	virtual void SpawnRenderActor() PURE_VIRTUAL(URenderActorWidget::SpawnRenderActor, );
+	virtual ARenderActor* GetRenderActor() const PURE_VIRTUAL(URenderActorWidget::GetRenderActor, return nullptr;);
+
+	void ConfigureRenderActor();
+	void BeginRenderCapture();
+	void UpdateRender();
+	void StopRenderCapture();
+	
+	UPROPERTY(meta=(BindWidget))
+	UImage* DisplayImage;
+
+	UPROPERTY(meta=(BindWidget))
+	USizeBox* RenderSizeBox;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	FName DisplayImageRenderTargetParamName = "RenderTarget";
+
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	FVector2D RenderSize;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	int FrameRate = 24;		// 渲染帧率
+
+	float RenderTickInterval;
+	FTimerHandle RenderTickTimerHandle;
+
+	UPROPERTY()
+	UTextureRenderTarget2D* RenderTarget;
+	
+};
