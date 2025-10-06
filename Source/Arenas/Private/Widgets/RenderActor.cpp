22e022760642b21b00b0b90191905aada709bdc9
invi <2267661826@qq.com>
2025-10-06 13:17:33 +0800

    创建渲染 Actor 类

---
+++ Source/Arenas/Private/Widgets/RenderActor.cpp
@@ -0,0 +1,48 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "RenderActor.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+
+
+// Sets default values
+ARenderActor::ARenderActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	SetRootComponent(RootSceneComponent);
+	SceneCaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("SceneCaptureComponent"));
+	SceneCaptureComponent->SetupAttachment(RootSceneComponent);
+
+	SceneCaptureComponent->bCaptureEveryFrame = false;	// 不需要每帧捕获
+	SceneCaptureComponent->FOVAngle = 30.f;	// 设置视野角度
+	
+	
+}
+
+void ARenderActor::SetRenderTarget(UTextureRenderTarget2D* NewRenderTarget)
+{
+	SceneCaptureComponent->TextureTarget = NewRenderTarget;
+}
+
+void ARenderActor::UpdateRender()
+{
+	if (IsValid(SceneCaptureComponent))
+	{
+		SceneCaptureComponent->CaptureScene();
+	}
+}
+
+// Called when the game starts or when spawned
+void ARenderActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 只显示该Actor的组件，环境中其他物体都不显示
+	SceneCaptureComponent->ShowOnlyActorComponents(this);
+	
+}
+
+
---
+++ Source/Arenas/Private/Widgets/RenderActor.h
@@ -0,0 +1,40 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RenderActor.generated.h"
+
+/**
+ * 该类用于在UI中渲染3D模型，或者其他不会被渲染到场景中的物体
+ */
+
+class USceneComponent;
+class USceneCaptureComponent2D;
+class UTextureRenderTarget2D;
+
+UCLASS()
+class ARENAS_API ARenderActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARenderActor();
+
+	void SetRenderTarget(UTextureRenderTarget2D* NewRenderTarget);
+	void UpdateRender();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Render Actor")
+	USceneComponent* RootSceneComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Render Actor")
+	USceneCaptureComponent2D* SceneCaptureComponent;
+	
+};
