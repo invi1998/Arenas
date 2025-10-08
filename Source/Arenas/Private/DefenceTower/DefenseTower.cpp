a7f5b1a2d6c6361c6c403e3844ec331a2cf7a11b
invi <2267661826@qq.com>
2025-10-08 14:49:13 +0800

    防御塔

---
+++ Source/Arenas/Private/DefenceTower/DefenseTower.cpp
@@ -0,0 +1,17 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "DefenseTower.h"
+
+
+ADefenseTower::ADefenseTower()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ADefenseTower::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Arenas/Private/DefenceTower/DefenseTower.h
@@ -0,0 +1,22 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Character.h"
+#include "DefenseTower.generated.h"
+
+UCLASS()
+class ARENAS_API ADefenseTower : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ADefenseTower();
+
+protected:
+	virtual void BeginPlay() override;
+	
+
+};
