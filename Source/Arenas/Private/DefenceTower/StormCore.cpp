84429b9b2a682540d78c0445be76958af298eb4d
invi <2267661826@qq.com>
2025-10-17 02:06:18 +0800

    基地核心

---
+++ Source/Arenas/Private/DefenceTower/StormCore.cpp
@@ -0,0 +1,54 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "StormCore.h"
+
+#include "AI/ArenasTowerAIController.h"
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+AStormCore::AStormCore()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ViewCam = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCam"));
+	ViewCam->SetupAttachment(GetRootComponent());
+}
+
+void AStormCore::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	CoreAIController = Cast<AArenasTowerAIController>(NewController);
+}
+
+void AStormCore::SetGenericTeamId(const FGenericTeamId& InTeamID)
+{
+	Super::SetGenericTeamId(InTeamID);
+
+	if (CoreAIController)
+	{
+		CoreAIController->SetGenericTeamId(InTeamID);
+	}
+}
+
+// Called when the game starts or when spawned
+void AStormCore::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStormCore::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AStormCore::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Arenas/Private/DefenceTower/StormCore.h
@@ -0,0 +1,42 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ArenasCharacter.h"
+#include "StormCore.generated.h"
+
+class AArenasTowerAIController;
+class UCameraComponent;
+
+UCLASS()
+class ARENAS_API AStormCore : public AArenasCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AStormCore();
+
+	virtual void PossessedBy(AController* NewController) override;
+
+	virtual void SetGenericTeamId(const FGenericTeamId& InTeamID) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Detection")
+	UCameraComponent* ViewCam;
+
+	UPROPERTY()
+	AArenasTowerAIController* CoreAIController;
+};
