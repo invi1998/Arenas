09f144bda50bac97bbeffe88338c5b9e9031865f
invi <2267661826@qq.com>
2025-10-20 14:15:50 +0800

    创建一个游戏玩法菜单小部件类

---
+++ Source/Arenas/Private/Widgets/Menu/GameplayMenu.cpp
@@ -0,0 +1,31 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "GameplayMenu.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+#include "Widgets/Component/ArenasButton.h"
+
+void UGameplayMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	MainMenuButton->ButtonArea->OnClicked.AddDynamic(this, &UGameplayMenu::OnMainMenuButtonClicked);
+	QuitGameButton->ButtonArea->OnClicked.AddDynamic(this, &UGameplayMenu::OnQuitGameButtonClicked);
+	
+}
+
+FOnButtonClickedEvent& UGameplayMenu::GetResumeButtonClickedEventDelegate() const
+{
+	return ResumeButton->ButtonArea->OnClicked;
+}
+
+void UGameplayMenu::OnMainMenuButtonClicked()
+{
+	
+}
+
+void UGameplayMenu::OnQuitGameButtonClicked()
+{
+	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
---
+++ Source/Arenas/Private/Widgets/Menu/GameplayMenu.h
@@ -0,0 +1,45 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "GameplayMenu.generated.h"
+
+class UArenasButton;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UGameplayMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeConstruct() override;
+
+	FOnButtonClickedEvent& GetResumeButtonClickedEventDelegate() const;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* MenuTitle;
+
+	UPROPERTY(meta=(BindWidget))
+	UArenasButton* ResumeButton;	// 继续游戏按钮
+
+	UPROPERTY(meta=(BindWidget))
+	UArenasButton* MainMenuButton;	// 返回主菜单按钮
+
+	UPROPERTY(meta=(BindWidget))
+	UArenasButton* QuitGameButton;	// 退出游戏按钮
+
+	UFUNCTION()
+	void OnMainMenuButtonClicked();
+
+	UFUNCTION()
+	void OnQuitGameButtonClicked();
+	
+};
