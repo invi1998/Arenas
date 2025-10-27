3e71c08cf308c66f0500f80f5bc30b6248d7275d
invi <2267661826@qq.com>
2025-10-27 21:52:12 +0800

    黑洞技能

---
+++ Source/Arenas/Private/GAS/Abilities/ArenasGA_BlackHole.cpp
@@ -0,0 +1,75 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "ArenasGA_BlackHole.h"
+
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
+#include "GAS/TargetActor/TargetActor_GroundPick.h"
+
+void UArenasGA_BlackHole::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	if (!HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		K2_EndAbility();
+		return;
+	}
+
+	PlayCastBlackHoleMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, TargetingMontage);
+	PlayCastBlackHoleMontageTask->OnCompleted.AddDynamic(this, &UArenasGA_BlackHole::K2_EndAbility);
+	PlayCastBlackHoleMontageTask->OnInterrupted.AddDynamic(this, &UArenasGA_BlackHole::K2_EndAbility);
+	PlayCastBlackHoleMontageTask->OnCancelled.AddDynamic(this, &UArenasGA_BlackHole::K2_EndAbility);
+	PlayCastBlackHoleMontageTask->OnBlendOut.AddDynamic(this, &UArenasGA_BlackHole::K2_EndAbility);
+	PlayCastBlackHoleMontageTask->ReadyForActivation();
+
+	UAbilityTask_WaitTargetData* WaitPlacementTask = UAbilityTask_WaitTargetData::WaitTargetData(this, NAME_None, EGameplayTargetingConfirmation::UserConfirmed, GroundPickTargetActorClass);
+	WaitPlacementTask->ValidData.AddDynamic(this, &UArenasGA_BlackHole::PlaceBlackHole);
+	WaitPlacementTask->Cancelled.AddDynamic(this, &UArenasGA_BlackHole::PlacementCancelled);
+	WaitPlacementTask->ReadyForActivation();
+
+	AGameplayAbilityTargetActor* TargetActor;
+	WaitPlacementTask->BeginSpawningActor(this, GroundPickTargetActorClass, TargetActor);
+
+	if (ATargetActor_GroundPick* TargetActor_GroundPick = Cast<ATargetActor_GroundPick>(TargetActor))
+	{
+		TargetActor_GroundPick->SetShouldDrawDebugSphere(bShowDebugLine);
+		TargetActor_GroundPick->SetTargetAreaRadius(TargetAreaRadius);
+		TargetActor_GroundPick->SetTargetTraceDistance(TargetTraceRange);
+	}
+	WaitPlacementTask->FinishSpawningActor(this, TargetActor);
+	
+}
+
+void UArenasGA_BlackHole::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+
+	RemoveAimEffect();
+	
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UArenasGA_BlackHole::PlaceBlackHole(const FGameplayAbilityTargetDataHandle& Data)
+{
+}
+
+void UArenasGA_BlackHole::PlacementCancelled(const FGameplayAbilityTargetDataHandle& Data)
+{
+}
+
+void UArenasGA_BlackHole::AddAimEffect()
+{
+	if (!AimEffectClass) return;
+	ActiveAimGameplayEffectHandle = BP_ApplyGameplayEffectToOwner(AimEffectClass);
+}
+
+void UArenasGA_BlackHole::RemoveAimEffect()
+{
+	if (ActiveAimGameplayEffectHandle.IsValid())
+	{
+		BP_RemoveGameplayEffectFromOwnerWithHandle(ActiveAimGameplayEffectHandle);	
+	}
+}
---
+++ Source/Arenas/Private/GAS/Abilities/ArenasGA_BlackHole.h
@@ -0,0 +1,54 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArenasGameplayAbility.h"
+#include "ArenasGA_BlackHole.generated.h"
+
+class UAbilityTask_PlayMontageAndWait;
+class ATargetActor_GroundPick;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UArenasGA_BlackHole : public UArenasGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Targeting")
+	float TargetAreaRadius = 500.f;		// 目标区域半径
+
+	UPROPERTY(EditDefaultsOnly, Category = "Targeting")
+	float TargetTraceRange = 2000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Targeting")
+	TSubclassOf<ATargetActor_GroundPick> GroundPickTargetActorClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UAnimMontage* TargetingMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Effect")
+	TSubclassOf<UGameplayEffect> AimEffectClass;		// 瞄准特效GameplayEffect类
+
+	FActiveGameplayEffectHandle ActiveAimGameplayEffectHandle;
+
+	UPROPERTY()
+	UAbilityTask_PlayMontageAndWait* PlayCastBlackHoleMontageTask;
+
+	UFUNCTION()
+	void PlaceBlackHole(const FGameplayAbilityTargetDataHandle& Data);		// 放置黑洞
+
+	UFUNCTION()
+	void PlacementCancelled(const FGameplayAbilityTargetDataHandle& Data);	// 放置取消
+
+	void AddAimEffect();
+	void RemoveAimEffect();
+	
+};
