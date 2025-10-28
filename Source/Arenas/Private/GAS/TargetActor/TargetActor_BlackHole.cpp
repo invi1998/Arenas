7c04eeba777b4335eb9b3f48ce943d75a73beb8d
invi <2267661826@qq.com>
2025-10-28 13:38:08 +0800

    ATargetActor_BlackHole

---
+++ Source/Arenas/Private/GAS/TargetActor/TargetActor_BlackHole.cpp
@@ -0,0 +1,107 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "TargetActor_BlackHole.h"
+
+#include "Net/UnrealNetwork.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/SphereComponent.h"
+
+
+
+
+// Sets default values
+ATargetActor_BlackHole::ATargetActor_BlackHole()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	DetectionSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphereComponent"));
+	DetectionSphereComponent->SetupAttachment(GetRootComponent());
+	DetectionSphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	DetectionSphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap); // 只与Pawn通道重叠检测
+	DetectionSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ATargetActor_BlackHole::ActorInBlackHoleArea);
+	DetectionSphereComponent->OnComponentEndOverlap.AddDynamic(this, &ATargetActor_BlackHole::ActorOutBlackHoleArea);
+
+	VFXComp = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("VFXComp"));
+	VFXComp->SetupAttachment(GetRootComponent());
+
+	bReplicates = true;
+	ShouldProduceTargetDataOnServer = true; // 服务器端生成目标数据，而客户端只是进行模拟和显示
+}
+
+// Called every frame
+void ATargetActor_BlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATargetActor_BlackHole::ConfigureBlackHoleTargetActor(float InBlackHoleRadius, float InPullSpeed,
+	float InBlackHoleDuration, const FGenericTeamId& OwningTeamId)
+{
+	BlackHoleRadius = InBlackHoleRadius;
+	PullSpeed = InPullSpeed;
+	SetGenericTeamId(OwningTeamId);
+	BlackHoleDuration = InBlackHoleDuration;
+
+	DetectionSphereComponent->SetSphereRadius(BlackHoleRadius);
+
+
+
+}
+
+void ATargetActor_BlackHole::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(ATargetActor_BlackHole, BlackHoleRadius, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME(ATargetActor_BlackHole, TeamId);
+	
+}
+
+void ATargetActor_BlackHole::SetGenericTeamId(const FGenericTeamId& TeamID)
+{
+	TeamId = TeamID;
+}
+
+FGenericTeamId ATargetActor_BlackHole::GetGenericTeamId() const
+{
+	return TeamId;
+}
+
+ETeamAttitude::Type ATargetActor_BlackHole::GetTeamAttitudeTowards(const AActor& Other) const
+{
+	if (const IGenericTeamAgentInterface* OtherTeamAgent = Cast<IGenericTeamAgentInterface>(&Other))
+	{
+		FGenericTeamId OtherTeamID = OtherTeamAgent->GetGenericTeamId();
+		FGenericTeamId MyTeamID = GetGenericTeamId();
+
+		if (MyTeamID == OtherTeamID) 
+			return ETeamAttitude::Friendly;
+		else if (MyTeamID == FGenericTeamId::NoTeam || OtherTeamID == FGenericTeamId::NoTeam)
+			return ETeamAttitude::Neutral;
+		else
+			return ETeamAttitude::Hostile;
+	}
+	return ETeamAttitude::Neutral;
+}
+
+void ATargetActor_BlackHole::OnRep_BlackHoleRadius()
+{
+	DetectionSphereComponent->SetSphereRadius(BlackHoleRadius);
+}
+
+void ATargetActor_BlackHole::ActorInBlackHoleArea(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ATargetActor_BlackHole::ActorOutBlackHoleArea(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
---
+++ Source/Arenas/Private/GAS/TargetActor/TargetActor_BlackHole.h
@@ -0,0 +1,60 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "TargetActor_BlackHole.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class ARENAS_API ATargetActor_BlackHole : public AGameplayAbilityTargetActor, public IGenericTeamAgentInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATargetActor_BlackHole();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void ConfigureBlackHoleTargetActor(float InBlackHoleRadius, float InPullSpeed, float InBlackHoleDuration, const FGenericTeamId& OwningTeamId);
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void SetGenericTeamId(const FGenericTeamId& TeamID) override;
+	virtual FGenericTeamId GetGenericTeamId() const override;
+	virtual ETeamAttitude::Type GetTeamAttitudeTowards(const AActor& Other) const override;
+	
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	USceneComponent* RootComp;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	USphereComponent* DetectionSphereComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	UParticleSystemComponent* VFXComp;
+
+	UPROPERTY(ReplicatedUsing = OnRep_BlackHoleRadius)
+	float BlackHoleRadius;		// 黑洞半径
+
+	float PullSpeed;					// 吸引速度
+	float BlackHoleDuration;			// 黑洞持续时间
+
+	UPROPERTY(Replicated)
+	FGenericTeamId TeamId;
+
+	UFUNCTION()
+	void OnRep_BlackHoleRadius();
+
+	UFUNCTION()
+	void ActorInBlackHoleArea(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void ActorOutBlackHoleArea(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	
+};
