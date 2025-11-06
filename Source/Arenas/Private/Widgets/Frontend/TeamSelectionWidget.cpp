1520dd5eb94ec92b01504aabcab1ee791d0e5bab
invi <2267661826@qq.com>
2025-11-07 00:41:28 +0800

    创建队伍选择槽窗口小部件

---
+++ Source/Arenas/Private/Widgets/Frontend/TeamSelectionWidget.cpp
@@ -0,0 +1,30 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "TeamSelectionWidget.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UTeamSelectionWidget::SetSlotID(uint8 InSlotID)
+{
+	SlotID = InSlotID;
+}
+
+void UTeamSelectionWidget::UpdateSlotInfoText(const FString& PlayerNickName)
+{
+	InfoTextBlock->SetText(FText::FromString(PlayerNickName));
+}
+
+void UTeamSelectionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SelectButton->OnClicked.AddDynamic(this, &UTeamSelectionWidget::OnSelectButtonClicked);
+	
+}
+
+void UTeamSelectionWidget::OnSelectButtonClicked()
+{
+	OnSlotClicked.Broadcast(SlotID);
+}
---
+++ Source/Arenas/Private/Widgets/Frontend/TeamSelectionWidget.h
@@ -0,0 +1,41 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TeamSelectionWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnSlotClickedDelegate, uint8 /*SlotID*/);
+
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UTeamSelectionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetSlotID(uint8 InSlotID);
+	void UpdateSlotInfoText(const FString& PlayerNickName);
+	virtual void NativeConstruct() override;
+	
+	FOnSlotClickedDelegate OnSlotClicked;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UButton* SelectButton;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* InfoTextBlock;
+
+	UFUNCTION()
+	void OnSelectButtonClicked();
+
+	uint8 SlotID;
+	
+};
