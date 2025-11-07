6e8c143c1199fe49198a518c53de35646fcafc3b
invi <2267661826@qq.com>
2025-11-08 01:38:25 +0800

    Add the Character Defination Primary Asset

---
+++ Source/Arenas/Private/Character/PA_CharacterDefinition.cpp
@@ -0,0 +1,60 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "PA_CharacterDefinition.h"
+
+#include "ArenasCharacter.h"
+
+FPrimaryAssetId UPA_CharacterDefinition::GetPrimaryAssetId() const
+{
+	return FPrimaryAssetId(GetCharacterDefinitionAssetType(), GetFName());
+}
+
+FPrimaryAssetType UPA_CharacterDefinition::GetCharacterDefinitionAssetType()
+{
+	return FPrimaryAssetType("CharacterDefinition");
+}
+
+UTexture2D* UPA_CharacterDefinition::LoadCharacterIcon() const
+{
+	// 同步加载角色图标资源
+	CharacterIcon.LoadSynchronous();
+	if (CharacterIcon.IsValid())
+	{
+		return CharacterIcon.Get();
+	}
+	
+	return nullptr;
+}
+
+TSubclassOf<AArenasCharacter> UPA_CharacterDefinition::LoadCharacterClass() const
+{
+	CharacterClass.LoadSynchronous();
+	if (CharacterClass.IsValid())
+	{
+		return CharacterClass.Get();
+	}
+	return TSubclassOf<AArenasCharacter>();
+}
+
+TSubclassOf<UAnimInstance> UPA_CharacterDefinition::LoadCharacterDisplayAnimBP() const
+{
+	CharacterDisplayAnimBP.LoadSynchronous();
+	if (CharacterDisplayAnimBP.IsValid())
+	{
+		return CharacterDisplayAnimBP.Get();
+	}
+	return TSubclassOf<UAnimInstance>();
+}
+
+USkeletalMesh* UPA_CharacterDefinition::LoadDisplaySkeletalMesh() const
+{
+	TSubclassOf<AArenasCharacter> CharacterBPClass = LoadCharacterClass();
+	if (!CharacterBPClass) return nullptr;
+
+	AArenasCharacter* CharacterCDO = CharacterBPClass->GetDefaultObject<AArenasCharacter>();
+	if (!CharacterCDO) return nullptr;
+
+	return CharacterCDO->GetMesh()->GetSkeletalMeshAsset();
+	
+}
---
+++ Source/Arenas/Private/Character/PA_CharacterDefinition.h
@@ -0,0 +1,43 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PA_CharacterDefinition.generated.h"
+
+class AArenasCharacter;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UPA_CharacterDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+	static FPrimaryAssetType GetCharacterDefinitionAssetType();
+	
+	FString GetCharacterName() const { return CharacterName; }
+	UTexture2D* LoadCharacterIcon() const;
+	TSubclassOf<AArenasCharacter> LoadCharacterClass() const;
+	TSubclassOf<UAnimInstance> LoadCharacterDisplayAnimBP() const;
+	USkeletalMesh* LoadDisplaySkeletalMesh() const;
+	
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	FString CharacterName;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftObjectPtr<UTexture2D> CharacterIcon;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftClassPtr<AArenasCharacter> CharacterClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftClassPtr<UAnimInstance> CharacterDisplayAnimBP;
+
+	
+};
