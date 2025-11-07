9e906e4b97dc3bab5eb2a0e32172f30cfcb803ad
invi <2267661826@qq.com>
2025-11-07 15:37:51 +0800

    创建玩家选择结构

---
+++ Source/Arenas/Private/Types/PlayerInfoTypes.cpp
@@ -0,0 +1,46 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "PlayerInfoTypes.h"
+
+#include "GameFramework/PlayerState.h"
+#include "Network/ArenasNetFunctionLibrary.h"
+
+FPlayerInfoData::FPlayerInfoData() : Slot(GetInvalidSlot()), PlayerUniqueId{FUniqueNetIdRepl::Invalid()}, PlayerNickName(TEXT(""))
+{
+}
+
+FPlayerInfoData::FPlayerInfoData(uint8 InSlot, const APlayerState* InPlayerState)
+	: Slot(InSlot)
+{
+	if (InPlayerState)
+	{
+		PlayerUniqueId = InPlayerState->GetUniqueId();
+		PlayerNickName = InPlayerState->GetPlayerName();
+	}
+}
+
+bool FPlayerInfoData::IsForPlayer(const APlayerState* InPlayerState) const
+{
+	if (!InPlayerState) return false;
+
+#if WITH_EDITOR
+	// 如果是在编辑器中，则使用昵称进行比较，以便测试时更方便识别玩家
+	return PlayerNickName == InPlayerState->GetPlayerName();
+#else
+	return PlayerUniqueId == InPlayerState->GetUniqueId();
+#endif
+
+	
+}
+
+bool FPlayerInfoData::IsValid() const
+{
+#if WITH_EDITOR
+	return true;
+#else
+	return Slot != GetInvalidSlot()
+		&& PlayerUniqueId.IsValid()
+		&& Slot < UArenasNetFunctionLibrary::GetPlayerCounterPerTeam() * 2;
+#endif
+}
---
+++ Source/Arenas/Private/Types/PlayerInfoTypes.h
@@ -0,0 +1,47 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerInput.generated.h"
+
+/**
+ * 
+ */
+
+class APlayerState;
+
+USTRUCT()
+struct FPlayerInfoData
+{
+	GENERATED_BODY()
+
+public:
+	FPlayerInfoData();
+	FPlayerInfoData(uint8 InSlot, const APlayerState* InPlayerState);
+
+	static uint8 GetInvalidSlot() { return 255; }
+
+	FORCEINLINE void SetSlot(uint8 InSlot) { Slot = InSlot; }
+	FORCEINLINE uint8 GetSlot() const { return Slot; }
+	FORCEINLINE FUniqueNetIdRepl GetPlayerUniqueId() const { return PlayerUniqueId; }
+	FORCEINLINE FString GetPlayerNickName() const { return PlayerNickName; }
+
+	bool IsForPlayer(const APlayerState* InPlayerState) const;
+	bool IsValid() const;
+
+private:
+	UPROPERTY()
+	uint8 Slot;
+
+	UPROPERTY()
+	FUniqueNetIdRepl PlayerUniqueId;		// 玩家唯一ID（支持网络复制）
+
+	UPROPERTY()
+	FString PlayerNickName;				// 玩家昵称
+
+	
+	
+};
+
+
