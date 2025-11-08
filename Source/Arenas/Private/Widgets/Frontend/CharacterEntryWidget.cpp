eb8773f95df10f8597d1fb0886ecd82d54f1af34
invi <2267661826@qq.com>
2025-11-08 20:50:47 +0800

    UCharacterEntryWidget

---
+++ Source/Arenas/Private/Widgets/Frontend/CharacterEntryWidget.cpp
@@ -0,0 +1,26 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "CharacterEntryWidget.h"
+
+#include "Character/PA_CharacterDefinition.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UCharacterEntryWidget::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+
+	CharacterDefinition = Cast<UPA_CharacterDefinition>(ListItemObject);
+	if (CharacterDefinition)
+	{
+		CharacterIcon->GetDynamicMaterial()->SetTextureParameterValue(IconTextureMatParameterName, CharacterDefinition->LoadCharacterIcon());
+		CharacterNameText->SetText(FText::FromString(CharacterDefinition->GetCharacterName()));
+	}
+	
+}
+
+void UCharacterEntryWidget::SetSelectedState(bool bIsSelected)
+{
+	CharacterIcon->GetDynamicMaterial()->SetScalarParameterValue(SaturationMatParameterName, bIsSelected ? 0.0f : 1.0f);
+}
---
+++ Source/Arenas/Private/Widgets/Frontend/CharacterEntryWidget.h
@@ -0,0 +1,45 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterEntryWidget.generated.h"
+
+class UPA_CharacterDefinition;
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UCharacterEntryWidget : public UUserWidget, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+
+	void SetSelectedState(bool bIsSelected);
+	
+	FORCEINLINE const UPA_CharacterDefinition* GetCharacterDefinition() const { return CharacterDefinition; }
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UImage* CharacterIcon;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* CharacterNameText;
+
+	UPROPERTY(EditDefaultsOnly, Category="CharacterEntry")
+	FName IconTextureMatParameterName = "Icon";
+
+	UPROPERTY(EditDefaultsOnly, Category="CharacterEntry")
+	FName SaturationMatParameterName = "Saturation";		// 饱和度参数名称
+
+	UPROPERTY()
+	UPA_CharacterDefinition* CharacterDefinition;
+
+	
+};
