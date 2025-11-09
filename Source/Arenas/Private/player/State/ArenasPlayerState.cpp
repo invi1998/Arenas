9cc6f6cc46658b10bc0b442ca8099917ff9f5a81
invi <2267661826@qq.com>
2025-11-09 19:20:01 +0800

    添加玩家选择和角色选择的服务器功能

--- Source/Arenas/Private/player/State/ArenasPlayerState.cpp
+++ Source/Arenas/Private/player/State/ArenasPlayerState.cpp
@@ -2,3 +2,70 @@
 
 
 #include "ArenasPlayerState.h"
+
+#include "Framework/ArenasGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+
+AArenasPlayerState::AArenasPlayerState()
+{
+	bReplicates = true;
+	SetNetUpdateFrequency(100.f);
+}
+
+void AArenasPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CachedArenasGameState = Cast<AArenasGameState>(UGameplayStatics::GetGameState(this));
+
+	if (CachedArenasGameState)
+	{
+		CachedArenasGameState->OnPlayerSelectionChangedSignature.AddUObject(this, &AArenasPlayerState::OnPlayerSelectionChanged);
+	}
+	
+}
+
+void AArenasPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AArenasPlayerState, PlayerSelectionData);
+
+	
+}
+
+void AArenasPlayerState::Server_SetSelectedCharacter_Implementation(const UPA_CharacterDefinition* InSelectedCharacter)
+{
+	if (!CachedArenasGameState) return;
+
+	if (!InSelectedCharacter) return;
+
+	if (CachedArenasGameState->IsDefinitionSelected(InSelectedCharacter)) return;
+
+	if (PlayerSelectionData.GetSelectedCharacter())
+	{
+		// 取消之前选择的角色
+		CachedArenasGameState->DeSelectedCharacter(PlayerSelectionData.GetSelectedCharacter());
+	}
+
+	PlayerSelectionData.SetSelectedCharacter(InSelectedCharacter);
+	CachedArenasGameState->SetCharacterSelected(this, InSelectedCharacter);
+}
+
+bool AArenasPlayerState::Server_SetSelectedCharacter_Validate(const UPA_CharacterDefinition* InSelectedCharacter)
+{
+	return true;
+}
+
+void AArenasPlayerState::OnPlayerSelectionChanged(const TArray<FPlayerSelection>& InPlayerSelections)
+{
+	for (const FPlayerSelection& PlayerSelection : InPlayerSelections)
+	{
+		if (PlayerSelection.IsForPlayer(this))
+		{
+			PlayerSelectionData = PlayerSelection;
+			break;
+		}
+	}
+}
--- Source/Arenas/Private/player/State/ArenasPlayerState.h
+++ Source/Arenas/Private/player/State/ArenasPlayerState.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "Types/PlayerInfoTypes.h"
 #include "ArenasPlayerState.generated.h"
 
+class AArenasGameState;
+class UPA_CharacterDefinition;
 /**
  * 
  */
@@ -13,4 +16,27 @@ UCLASS()
 class ARENAS_API AArenasPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+	AArenasPlayerState();
+	
+	virtual void BeginPlay() override;
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// 客户端UI请求服务端更新英雄角色选择
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetSelectedCharacter(const UPA_CharacterDefinition* InSelectedCharacter);
+	
+	
+private:
+	UPROPERTY(Replicated)
+	FPlayerSelection PlayerSelectionData;
+
+	UPROPERTY()
+	AArenasGameState* CachedArenasGameState;
+
+	void OnPlayerSelectionChanged(const TArray<FPlayerSelection>& InPlayerSelections);
+
+	
 };
