8c6eaffe920d3c9acf908b266938fa79e7c47dff
invi <2267661826@qq.com>
2025-11-09 20:02:44 +0800

    创建角色显示类

---
+++ Source/Arenas/Private/Widgets/Frontend/CharacterDisplay.cpp
@@ -0,0 +1,40 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "CharacterDisplay.h"
+
+#include "Camera/CameraComponent.h"
+#include "Character/PA_CharacterDefinition.h"
+
+
+// Sets default values
+ACharacterDisplay::ACharacterDisplay()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>(FName("RootComponent")));
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(FName("SkeletalMeshComponent"));
+	SkeletalMeshComponent->SetupAttachment(GetRootComponent());
+
+	ViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(FName("ViewCameraComponent"));
+	ViewCameraComponent->SetupAttachment(GetRootComponent());
+
+	
+}
+
+void ACharacterDisplay::ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* InCharacterDefinition)
+{
+	if (!InCharacterDefinition) return;
+
+	SkeletalMeshComponent->SetSkeletalMeshAsset(InCharacterDefinition->LoadDisplaySkeletalMesh());
+	SkeletalMeshComponent->SetAnimationMode(EAnimationMode::Type::AnimationBlueprint);
+	SkeletalMeshComponent->SetAnimInstanceClass(InCharacterDefinition->LoadCharacterDisplayAnimBP());
+	
+}
+
+
+
+
+
---
+++ Source/Arenas/Private/Widgets/Frontend/CharacterDisplay.h
@@ -0,0 +1,32 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterDisplay.generated.h"
+
+class UPA_CharacterDefinition;
+class UCameraComponent;
+
+UCLASS()
+class ARENAS_API ACharacterDisplay : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACharacterDisplay();
+
+	void ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* InCharacterDefinition);
+	
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	USkeletalMeshComponent* SkeletalMeshComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	UCameraComponent* ViewCameraComponent;
+	
+	
+
+};
