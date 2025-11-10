f6095c4ab6bbdf594969eb99d826da641c0ebb94
invi <2267661826@qq.com>
2025-11-10 16:47:41 +0800

    技能ToolTip

---
+++ Source/Arenas/Private/Widgets/Component/AbilityToolTip.cpp
@@ -0,0 +1,23 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "AbilityToolTip.h"
+
+#include "Components/Image.h"
+#include "Components/RichTextBlock.h"
+#include "Components/TextBlock.h"
+
+void UAbilityToolTip::SetAbilityInfo(UTexture2D* InAbilityIcon, const FString& InAbilityName,
+                                     const FString& InAbilityDescription, const FString& InAbilityCooldown, const FString& InAbilityManaCost,
+                                     const FString& InAbilityEffects, const FString& InAbilitySubDescription)
+{
+
+	AbilityIconImage->SetBrushFromTexture(InAbilityIcon);
+	AbilityNameText->SetText(FText::FromString(InAbilityName));
+	AbilityDescriptionText->SetText(FText::FromString(InAbilityDescription));
+	AbilityCooldownText->SetText(FText::FromString(InAbilityCooldown));
+	AbilityManaCostText->SetText(FText::FromString(InAbilityManaCost));
+	AbilityEffectsRichText->SetText(FText::FromString(InAbilityEffects));
+	AbilitySubDescriptionText->SetText(FText::FromString(InAbilitySubDescription));
+	
+}
---
+++ Source/Arenas/Private/Widgets/Component/AbilityToolTip.h
@@ -0,0 +1,45 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityToolTip.generated.h"
+
+class URichTextBlock;
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UAbilityToolTip : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetAbilityInfo(UTexture2D* InAbilityIcon, const FString& InAbilityName, const FString& InAbilityDescription, const FString& InAbilityCooldown, const FString& InAbilityManaCost, const FString& InAbilityEffects, const FString& InAbilitySubDescription);
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UImage* AbilityIconImage;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityNameText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityDescriptionText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityCooldownText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilityManaCostText;
+
+	UPROPERTY(meta=(BindWidget))
+	URichTextBlock* AbilityEffectsRichText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* AbilitySubDescriptionText;
+	
+};
