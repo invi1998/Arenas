53d6f73a764e6891da7f068e9a464c8c1bfe4985
invi <2267661826@qq.com>
2025-11-10 21:54:52 +0800

    UPlayerTeamSlotWidget

---
+++ Source/Arenas/Private/Widgets/Frontend/PlayerTeamSlotWidget.h
@@ -0,0 +1,27 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerTeamSlotWidget.generated.h"
+
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UPlayerTeamSlotWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	UImage* PlayerCharacterImage;		// 玩家角色图像
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* NameTextBlock;
+	
+	
+};
