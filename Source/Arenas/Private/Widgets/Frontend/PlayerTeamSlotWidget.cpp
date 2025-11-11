dc9e89e3a105a6e18f49c7f33111595570f5bfc1
invi <2267661826@qq.com>
2025-11-11 08:53:30 +0800

    实现玩家队伍槽小部件

--- Source/Arenas/Private/Widgets/Frontend/PlayerTeamSlotWidget.cpp
+++ Source/Arenas/Private/Widgets/Frontend/PlayerTeamSlotWidget.cpp
@@ -2,3 +2,59 @@
 
 
 #include "PlayerTeamSlotWidget.h"
+
+#include "Character/PA_CharacterDefinition.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UPlayerTeamSlotWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	CachedCharacterNameString = TEXT("...");
+	PlayerCharacterImage->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyIconMaterialParamName, 1.f);
+
+	
+}
+
+void UPlayerTeamSlotWidget::NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseEnter(InGeometry, InMouseEvent);
+	NameTextBlock->SetText(FText::FromString(CachedCharacterNameString));
+	PlayAnimationForward(HoveredAnimation);
+}
+
+void UPlayerTeamSlotWidget::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+	NameTextBlock->SetText(FText::FromString(CachedPlayerNameString));
+	PlayAnimationReverse(HoveredAnimation);
+}
+
+void UPlayerTeamSlotWidget::UpdateSlot(const FString& InPlayerName, const UPA_CharacterDefinition* InCharacterDef)
+{
+	CachedPlayerNameString = InPlayerName;
+	if (InCharacterDef)
+	{
+		CachedCharacterNameString = InCharacterDef->GetCharacterName();
+		PlayerCharacterImage->GetDynamicMaterial()->SetTextureParameterValue(CharacterIconMaterialParamName, InCharacterDef->LoadCharacterIcon());
+		PlayerCharacterImage->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyIconMaterialParamName, 0.f);
+	}
+	else
+	{
+		CachedCharacterNameString = TEXT("...");
+		PlayerCharacterImage->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyIconMaterialParamName, 1.f);
+	}
+}
+
+void UPlayerTeamSlotWidget::UpdateNameText()
+{
+	if (IsHovered())
+	{
+		NameTextBlock->SetText(FText::FromString(CachedCharacterNameString));
+	}
+	else
+	{
+		NameTextBlock->SetText(FText::FromString(CachedPlayerNameString));
+	}
+}
--- Source/Arenas/Private/Widgets/Frontend/PlayerTeamSlotWidget.h
+++ Source/Arenas/Private/Widgets/Frontend/PlayerTeamSlotWidget.h
@@ -6,6 +6,7 @@
 #include "Blueprint/UserWidget.h"
 #include "PlayerTeamSlotWidget.generated.h"
 
+class UPA_CharacterDefinition;
 class UTextBlock;
 class UImage;
 /**
@@ -16,12 +17,33 @@ class ARENAS_API UPlayerTeamSlotWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	virtual void NativeConstruct() override;
+	
+	virtual void NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
+	void UpdateSlot(const FString& InPlayerName, const UPA_CharacterDefinition* InCharacterDef);
+
 private:
 	UPROPERTY(meta=(BindWidget))
 	UImage* PlayerCharacterImage;		// 玩家角色图像
 
 	UPROPERTY(meta=(BindWidget))
 	UTextBlock* NameTextBlock;
-	
+
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	UWidgetAnimation* HoveredAnimation;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	FName CharacterIconMaterialParamName = "Icon";
+
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	FName CharacterEmptyIconMaterialParamName = "Empty";
+
+	FString CachedPlayerNameString;
+	FString CachedCharacterNameString;
+
+	void UpdateNameText();
 	
 };
