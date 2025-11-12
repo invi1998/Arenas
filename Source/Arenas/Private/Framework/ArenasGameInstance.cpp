82605d20433fb857ce907a27a2a05879a31d2090
invi <2267661826@qq.com>
2025-11-12 13:24:39 +0800

    在游戏实例类中实现地图加载

---
+++ Source/Arenas/Private/Framework/ArenasGameInstance.cpp
@@ -0,0 +1,25 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "ArenasGameInstance.h"
+
+void UArenasGameInstance::StartMatch()
+{
+	if (GetWorld()->GetNetMode() == ENetMode::NM_DedicatedServer || GetWorld()->GetNetMode() == ENetMode::NM_ListenServer)
+	{
+		// 如果是在专用服务器上运行，或者是监听服务器，则加载关卡并监听
+		LoadLevelAndListen(GameLevelMap);
+	}
+}
+
+void UArenasGameInstance::LoadLevelAndListen(const TSoftObjectPtr<UWorld>& LevelToLoad)
+{
+	// 获取关卡包名
+	const FName LevelURL = FName(FPackageName::ObjectPathToPackageName(LevelToLoad.ToString()));
+
+	if (!LevelURL.IsNone())
+	{
+		GetWorld()->ServerTravel(LevelURL.ToString() + "?listen");	// 加载关卡并监听
+	}
+	
+}
---
+++ Source/Arenas/Private/Framework/ArenasGameInstance.h
@@ -0,0 +1,33 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ArenasGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UArenasGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	void StartMatch();
+
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> MainMenuMap;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> LobbyMap;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> GameLevelMap;
+
+	void LoadLevelAndListen(const TSoftObjectPtr<UWorld>& LevelToLoad);
+	
+};
