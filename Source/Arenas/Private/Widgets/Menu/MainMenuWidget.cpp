f2fee86118a5e9cfb643551a94c342d44a61a67a
invi <2267661826@qq.com>
2025-11-18 12:55:45 +0800

    创建登录控件

---
+++ Source/Arenas/Private/Widgets/Menu/MainMenuWidget.cpp
@@ -0,0 +1,38 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+
+#include "MainMenuWidget.h"
+
+#include "Widgets/Component/ArenasButton.h"
+#include "Framework/ArenasGameInstance.h"
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	OwnerArenasGameInstance = GetGameInstance<UArenasGameInstance>();
+	if (OwnerArenasGameInstance)
+	{
+		
+	}
+	
+	LoginButton->ButtonArea->OnClicked.AddDynamic(this, &UMainMenuWidget::OnLoginButtonClicked);
+	
+}
+
+void UMainMenuWidget::OnLoginButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("#### Login Button Clicked"));
+}
+
+void UMainMenuWidget::LoginCompleted(bool bWasSuccessful, const FString& PlayerNickname, const FString& ErrorMessage)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("#### Login Successful. Nickname: %s"), *PlayerNickname);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("#### Login Failed. Error: %s"), *ErrorMessage);
+	}
+}
---
+++ Source/Arenas/Private/Widgets/Menu/MainMenuWidget.h
@@ -0,0 +1,41 @@
+﻿// Ace of Arenas. (invi_1998 All Rights Reserved)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+class UArenasButton;
+class UArenasGameInstance;
+class UWidgetSwitcher;
+/**
+ * 
+ */
+UCLASS()
+class ARENAS_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	UWidgetSwitcher* MainSwitcher;
+	
+	UPROPERTY()
+	UArenasGameInstance* OwnerArenasGameInstance;
+	
+	UPROPERTY(meta=(BindWidget))
+	UWidget* LoginWidgetRoot;
+	
+	UPROPERTY(meta=(BindWidget))
+	UArenasButton* LoginButton;
+	
+	UFUNCTION()
+	void OnLoginButtonClicked();
+	
+	void LoginCompleted(bool bWasSuccessful, const FString& PlayerNickname, const FString& ErrorMessage);
+	
+};
